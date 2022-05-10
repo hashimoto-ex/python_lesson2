5ef9db323f4b6a42a4c116983a32c6b393b7e939
hashimoto <yuhassy205@gmail.com>
2022-05-10 17:46:58 +0900

    5/10

--- rensyu3.py
+++ rensyu3.py
@@ -89,14 +89,130 @@ def weekDemand(num):
     else:
         print(weeks[num])
 
-weekDemand(int(input('数(0-6)> ')))
+#weekDemand(int(input('数(0-6)> ')))
 
+#うるう年の判定
+def leapYear(year):
+    if year % 400 == 0:
+        print(str(year)+'年はうるう年です。')
+    elif year % 100 == 0:
+        print(str(year)+'年はうるう年ではありません。')
+    elif year % 4 == 0:
+        print(str(year)+'年はうるう年です。')
+    else:
+        print(str(year)+'年はうるう年ではありません。')
 
+#leapYear(int(input('西暦で年を入力して下さい> ')))
 
+#あなたの誕生日は何曜日
+def birthdayWeek(year,month,day):
+    weeks = {
+    0:'日曜日',1:'月曜日',2:'火曜日',3:'水曜日',4:'木曜日',5:'金曜日',6:'土曜日'
+    }
+    datestr = f'{year}年{month}月{day}日'
+    weekday = (year + (year // 4) - (year // 100) + (year // 400) + ((13*month+8) // 5) + day) % 7
+    print(f'{datestr}は{weeks[weekday]}です。')
 
+#birthdayWeek(int(input('年> ')),int(input('月> ')),int(input('日> ')))
 
+#ジャンケンするプログラム
+import random
 
+def janken(yours):
+    #Rock-paper-scissors
+    RPS = {
+        0:'グー', 1:'チョキ', 2:'パー'
+    }
+    judge = ['あなたの勝ちです。','あなたの負けです。','あいこです。']
 
+    print('ジャンケンポン!')
+    cpu = random.randint(0,2)
+    if yours == cpu:
+        result = 2
+        connect = 'も'
+    else:
+        connect = 'は'
+        if yours > cpu:
+            if yours == 2 and cpu == 0:
+                result = 0
+            else: result = 1
+        else:
+            if yours == 0 and cpu == 2:
+                result = 1
+            else: result = 0
+            
+    print(f'わたしは{RPS[cpu]}。あなた{connect}{RPS[yours]}。{judge[result]}')
+
+#janken(int(input('あなたは？(0:グー, 1:チョキ, 2:パー)> ')))
+
+#数の比較
+def numComparison(a,b,c):
+    numbers = [a,b,c]
+    new = sorted(numbers)
     
-    
+    print(new[0],new[1],new[2],sep=' ')   
 
+
+#numComparison(int(input('数1> ')),int(input('数2> ')),int(input('数3> ')))
+
+#べき乗計算
+# n = int(input('数> '))
+# fac = 1
+# for i in range(1,n+1) :
+#     fac *= i
+# print(str(n)+'! =',fac,sep=' ')
+
+#棒グラフ
+def barGraph(num):
+    print(f'{str(num)}:',('■'*num))
+
+#barGraph(int(input('数> ')))
+
+#棒グラフver2
+def barGraphs(num):
+    for n in range(1,num + 1):
+        print(f'{str(n)}:',('■'*n),sep=' ')
+
+#barGraphs(int(input('数> ')))
+
+#データ数のわからない集計
+def totalling(data):
+    counter,gokei = 0,0
+    while data >= 0 :
+        counter += 1
+        gokei += data
+        data = int(input('データ入力(負の数で終了)> '))
+    heikin = gokei/counter
+    print(f'データ数:,{counter},合計:,{gokei},平均:,{heikin}')
+
+#totalling(int(input('データ入力(負の数で終了)> ')))
+
+#借金を返済しよう
+def debtCalc(shakkin,riritsu,hensai):
+    total,month = 0,0
+    while shakkin > hensai :
+        month += 1
+        shakkin = shakkin*(1 + riritsu/12/100) - hensai
+        print(str(month)+'月: 返済額',hensai,'円','残り',\
+        int(shakkin),sep=' ')
+        total += hensai
+    month += 1
+    shakkinInt: int = int(shakkin*(1 + riritsu/12/100))
+    total += shakkinInt
+    print(f'{month}月: 返済額{shakkinInt}円これで完済。返済総額: \
+    {total}円',sep=' ')
+
+#debtCalc(int(input('借金> ')),float(input('年利率(%)> ')),int(input('返済額> ')))
+
+#モンテカルロ・シミュレーション 
+def monteCarlo(n):
+    nc = 0
+    for i in range(0,n) :
+        x = random.random()
+        y = random.random()
+        if x**2 + y**2 < 1 :
+            nc += 1
+    pi = 4.0 * nc / n
+    print('PI =',pi,sep=' ')
+
+monteCarlo(int(input('実行回数> '))) 
